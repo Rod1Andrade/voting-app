cf39cafd3f65bc866f5da691dc29566fc2151e90
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-07-21 10:15:11 -0300

    UPDATE app/Database - Connections: Hieranchy of classes in connection statement

--- backend/src/App/Database/Connection/Connection.php
+++ backend/src/App/Database/Connection/Connection.php
@@ -6,7 +6,43 @@ namespace Rodri\VotingApp\App\Database\Connection\Connection;
 
 use PDO;
 
-interface Connection
+/**
+ * Interface Connection
+ * @package Rodri\VotingApp\App\Database\Connection\Connection
+ */
+abstract class Connection
 {
-    public static function getConnection(): PDO;
+    protected static Connection $instance;
+    protected ?PDO $pdoInstance;
+
+    protected const OPTIONS = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+
+    /**
+     * Get a connection
+     * @return Connection
+     */
+    public abstract static function getConnection(): Connection;
+
+    /**
+     * Get Single Postgres PDO connection.
+     * @return PDO
+     */
+    public abstract function pdo(): PDO;
+
+    /**
+     * Private constructor to maintain singleton pattern.
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Private clone to maintain singleton pattern.
+     */
+    protected function __clone(): void
+    {
+    }
 }
--- backend/src/App/Database/Connection/PgConnection.php
+++ backend/src/App/Database/Connection/PgConnection.php
@@ -5,49 +5,35 @@ namespace Rodri\VotingApp\App\Database\Connection;
 
 
 use PDO;
+use Rodri\VotingApp\App\Database\Connection\Connection\Connection;
 
 /**
  * Connection - PgConnection
  * @package Rodri\VotingApp\App\Database
  * @author Rodrigo Andrade
  */
-class PgConnection implements Connection
+class PgConnection extends Connection
 {
-
     /**
      *
+     * @return Connection
      */
-    private const OPTIONS = [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ];
-    /**
-     * @var PDO|null
-     */
-    private static ?PDO $pdoInstance;
-
-    /**
-     * Private constructor to maintain singleton pattern.
-     */
-    private function __construct()
+    public static function getConnection(): PgConnection
     {
-    }
+        if (!self::$instance)
+            self::$instance = new PgConnection();
 
-    /**
-     * Private clone to maintain singleton pattern.
-     */
-    private function __clone(): void
-    {
+        return self::$instance;
     }
 
     /**
      * Get Single Postgres PDO connection.
      * @return PDO
      */
-    public static function getConnection(): PDO
+    public function pdo(): PDO
     {
-        if (!self::$pdoInstance) {
-            self::$pdoInstance = new PDO(
+        if (!self::getConnection()->pdoInstance) {
+            !self::getConnection()->pdoInstance = new PDO(
                 getenv('DB_DNS'),
                 getenv('DB_USER_NAME'),
                 getenv('DB_USER_PASS'),
@@ -55,6 +41,23 @@ class PgConnection implements Connection
             );
         }
 
-        return self::$pdoInstance;
+        return self::getConnection()->pdoInstance;
     }
+
+    /**
+     * Private constructor to maintain singleton pattern.
+     */
+    private function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Private clone to maintain singleton pattern.
+     */
+    protected function __clone(): void
+    {
+        parent::__clone();
+    }
+
 }
