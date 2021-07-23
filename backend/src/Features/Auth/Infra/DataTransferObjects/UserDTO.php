b23a489144ad5193d1b2732f0527ee00a9b79f57
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-07-23 08:42:51 -0300

    UPDATE - feature/register-user - Created DTO to transfer values between layers.

---
+++ backend/src/Features/Auth/Infra/DataTransferObjects/UserDTO.php
@@ -0,0 +1,165 @@
+<?php
+
+
+namespace Rodri\VotingApp\Features\Auth\Infra\DataTransferObjects;
+
+use DateTime;
+use Exception;
+use Rodri\VotingApp\Features\Auth\Domain\Entities\User;
+use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\BirthDate;
+use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\Email;
+use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\Password;
+use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\UserUuid;
+use RuntimeException;
+
+/**
+ * Class To Transfer data between layers - UserDTO
+ * @package Features\Auth\Infra\DataTrasferObjects
+ * @author Rodrigo Andrade
+ */
+class UserDTO
+{
+    private function __construct(
+        private string $userUuid,
+        private string $email,
+        private string $password,
+        private string $birthDate,
+        private string $name,
+        private string $lastName
+    )
+    {
+    }
+
+    /**
+     * Create a instance of User DTO based on User
+     * @param User $user
+     * @return UserDTO
+     */
+    public static function factoryUserDTO(User $user): UserDTO
+    {
+        return new UserDTO(
+            userUuid: $user->getUserUuid()->getValue(),
+            email: $user->getEmail()->getValue(),
+            password: $user->getPassword()->getValue(),
+            birthDate: $user->getBirthDate()->getFormattedDate(),
+            name: $user->getName(),
+            lastName: $user->getLastname()
+        );
+    }
+
+    /**
+     * @param UserDTO $userDTO
+     * @return User
+     */
+    public static function factoryUser(UserDTO $userDTO): User
+    {
+        try {
+            return new User(
+                userUuid: new UserUuid($userDTO->getUserUuid()),
+                email: new Email($userDTO->getEmail()),
+                password: new Password($userDTO->getPassword()),
+                birthDate: new BirthDate(new DateTime($userDTO->getBirthDate())),
+                name: $userDTO->getName(),
+                lastname: $userDTO->getLastName()
+            );
+        } catch (Exception $e) {
+            throw new RuntimeException($e);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserUuid(): string
+    {
+        return $this->userUuid;
+    }
+
+    /**
+     * @param string $userUuid
+     */
+    public function setUserUuid(string $userUuid): void
+    {
+        $this->userUuid = $userUuid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBirthDate(): string
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param string $birthDate
+     */
+    public function setBirthDate(string $birthDate): void
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+}
