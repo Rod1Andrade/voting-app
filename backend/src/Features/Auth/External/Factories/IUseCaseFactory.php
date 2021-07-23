9fe50f8226a502947da41a226a8a29a85762c472
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-07-23 09:11:25 -0300

    fix - feature/register-user - Factory of Use Cases.

--- backend/src/Features/Auth/External/Factories/IUseCaseFactory.php
+++ backend/src/Features/Auth/External/Factories/IUseCaseFactory.php
@@ -4,9 +4,10 @@
 namespace Rodri\VotingApp\Features\Auth\External\Factories;
 
 
+use Rodri\VotingApp\App\Database\Connection\Connection;
 use Rodri\VotingApp\Features\Auth\Domain\UseCases\IRegisterUserUseCase;
 
 interface IUseCaseFactory
 {
-    public static function registerUserUseCase(): IRegisterUserUseCase;
+    public static function registerUserUseCase(Connection $connection): IRegisterUserUseCase;
 }
--- backend/src/Features/Auth/External/Factories/UseCaseFactory.php
+++ backend/src/Features/Auth/External/Factories/UseCaseFactory.php
@@ -5,6 +5,7 @@ namespace Rodri\VotingApp\Features\Auth\External\Factories;
 
 
 use JetBrains\PhpStorm\Pure;
+use Rodri\VotingApp\App\Database\Connection\Connection;
 use Rodri\VotingApp\Features\Auth\Domain\UseCases\IRegisterUserUseCase;
 use Rodri\VotingApp\Features\Auth\External\Facades\RegisterUserUseCaseFacade;
 
@@ -15,8 +16,8 @@ class UseCaseFactory implements IUseCaseFactory
      * @return IRegisterUserUseCase
      * @codeCoverageIgnore
      */
-    #[Pure] public static function registerUserUseCase(): IRegisterUserUseCase
+    #[Pure] public static function registerUserUseCase(Connection $connection): IRegisterUserUseCase
     {
-        return (new RegisterUserUseCaseFacade())->createUseCase();
+        return (new RegisterUserUseCaseFacade())->createUseCase($connection);
     }
 }
