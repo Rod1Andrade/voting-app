49089c689dbd6b58df00379329d8b4b41dd5b6f1
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-07-27 11:56:52 -0300

    UPDATE - feature/authenticate-user - User DTO accept nullable values and match comprehensible with std class.

--- backend/src/Features/Auth/Infra/DataTransferObjects/UserDTO.php
+++ backend/src/Features/Auth/Infra/DataTransferObjects/UserDTO.php
@@ -5,13 +5,12 @@ namespace Rodri\VotingApp\Features\Auth\Infra\DataTransferObjects;
 
 use DateTime;
 use Exception;
+use JetBrains\PhpStorm\Pure;
 use Rodri\VotingApp\Features\Auth\Domain\Entities\User;
 use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\BirthDate;
 use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\Email;
 use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\Password;
 use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\UserUuid;
-use Rodri\VotingApp\Features\Auth\External\Adapters\PasswordEncrypt;
-use Rodri\VotingApp\Features\Auth\External\Adapters\Uuid;
 use RuntimeException;
 use stdClass;
 
@@ -23,14 +22,30 @@ use stdClass;
 class UserDTO
 {
     private function __construct(
-        private string $userUuid,
-        private string $email,
-        private string $password,
-        private string $birthDate,
-        private string $name,
-        private string $lastName
+        private ?string $userUuid,
+        private ?string $email,
+        private ?string $password,
+        private ?string $birthDate,
+        private ?string $name,
+        private ?string $lastName
     )
     {
+    }
+
+    /**
+     * @param array $user
+     * @return UserDTO
+     */
+    #[Pure] public static function factoryUserDTOFromArray(array $user): UserDTO
+    {
+        return new UserDTO(
+            userUuid: $user['userUuid'] ?? null,
+            email: $user['email'] ?? null,
+            password: $user['password'] ?? null,
+            birthDate: $user['birthDate'] ?? null,
+            name: $user['name'] ?? null,
+            lastName: $user['lastName'] ?? null
+        );
     }
 
     /**
@@ -38,15 +53,15 @@ class UserDTO
      * @param string $uuid
      * @return UserDTO
      */
-    public static function factoryUserDTOFromStdClass(stdClass $user, string $uuid = ''): UserDTO
+    #[Pure] public static function factoryUserDTOFromStdClass(stdClass $user): UserDTO
     {
         return new UserDTO(
-            userUuid: empty($uuid) ? Uuid::genUUIDv4() : $uuid,
-            email: $user->email,
-            password: PasswordEncrypt::hash($user->password),
-            birthDate: $user->birthDate,
-            name: $user->name,
-            lastName: $user->lastName
+            userUuid: $user->userUuid ?? $user->user_id ?? null, // TODO: Colocar uuid generator fora do dto
+            email: $user->email ?? null,
+            password: $user->password ?? null, // TODO: Colocar hash de senha fora do dto
+            birthDate: $user->birthDate ??null,
+            name: $user->name ?? null,
+            lastName: $user->lastName ?? null
         );
     }
     
@@ -58,10 +73,10 @@ class UserDTO
     public static function factoryUserDTOfromUser(User $user): UserDTO
     {
         return new UserDTO(
-            userUuid: $user->getUserUuid()->getValue(),
-            email: $user->getEmail()->getValue(),
-            password: $user->getPassword()->getValue(),
-            birthDate: $user->getBirthDate()->getFormattedDate(),
+            userUuid: $user->getUserUuid()?->getValue(),
+            email: $user->getEmail()?->getValue(),
+            password: $user->getPassword()?->getValue(),
+            birthDate: $user->getBirthDate()?->getFormattedDate(),
             name: $user->getName(),
             lastName: $user->getLastname()
         );
@@ -75,12 +90,12 @@ class UserDTO
     {
         try {
             return new User(
-                userUuid: new UserUuid($userDTO->getUserUuid()),
-                email: new Email($userDTO->getEmail()),
-                password: new Password(PasswordEncrypt::hash($userDTO->getPassword())),
-                birthDate: new BirthDate(new DateTime($userDTO->getBirthDate())),
-                name: $userDTO->getName(),
-                lastname: $userDTO->getLastName()
+                userUuid: $userDTO->userUuid ? new UserUuid($userDTO->getUserUuid()) : null,
+                email: $userDTO->email ? new Email($userDTO->getEmail()) : null,
+                password: $userDTO->password ? new Password($userDTO->getPassword()) : null,
+                birthDate: $userDTO->birthDate ? new BirthDate(new DateTime($userDTO->getBirthDate())) : null,
+                name: $userDTO->email ? $userDTO->getName(): null,
+                lastname: $userDTO->lastName ? $userDTO->getLastName() : null
             );
         } catch (Exception $e) {
             throw new RuntimeException($e);
@@ -88,98 +103,99 @@ class UserDTO
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getUserUuid(): string
+    public function getUserUuid(): ?string
     {
         return $this->userUuid;
     }
 
     /**
-     * @param string $userUuid
+     * @param string|null $userUuid
      */
-    public function setUserUuid(string $userUuid): void
+    public function setUserUuid(?string $userUuid): void
     {
         $this->userUuid = $userUuid;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getEmail(): string
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
     /**
-     * @param string $email
+     * @param string|null $email
      */
-    public function setEmail(string $email): void
+    public function setEmail(?string $email): void
     {
         $this->email = $email;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
 
     /**
-     * @param string $password
+     * @param string|null $password
      */
-    public function setPassword(string $password): void
+    public function setPassword(?string $password): void
     {
         $this->password = $password;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getBirthDate(): string
+    public function getBirthDate(): ?string
     {
         return $this->birthDate;
     }
 
     /**
-     * @param string $birthDate
+     * @param string|null $birthDate
      */
-    public function setBirthDate(string $birthDate): void
+    public function setBirthDate(?string $birthDate): void
     {
         $this->birthDate = $birthDate;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
 
     /**
-     * @param string $name
+     * @param string|null $name
      */
-    public function setName(string $name): void
+    public function setName(?string $name): void
     {
         $this->name = $name;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getLastName(): string
+    public function getLastName(): ?string
     {
         return $this->lastName;
     }
 
     /**
-     * @param string $lastName
+     * @param string|null $lastName
      */
-    public function setLastName(string $lastName): void
+    public function setLastName(?string $lastName): void
     {
         $this->lastName = $lastName;
     }
+
 }
