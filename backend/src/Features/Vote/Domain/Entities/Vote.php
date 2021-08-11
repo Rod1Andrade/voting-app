0ff09e373f9e44422201e810a94935c1d56cbac6
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-08-11 16:20:23 -0300

    REFACTORING - feature/vote - Type of value and return, added date time start and finish: #30

--- backend/src/Features/Vote/Domain/Entities/Vote.php
+++ backend/src/Features/Vote/Domain/Entities/Vote.php
@@ -2,8 +2,8 @@
 
 namespace Rodri\VotingApp\Features\Vote\Domain\Entities;
 
+use DateTime;
 use InvalidArgumentException;
-use Rodri\VotingApp\Features\Auth\Domain\ValueObjects\UserUuid;
 use Rodri\VotingApp\Features\VotingSection\Domain\ValueObjects\Subject;
 use Rodri\VotingApp\Features\VotingSection\Domain\ValueObjects\VotingUuid;
 
@@ -15,37 +15,22 @@ class Vote
 {
     private ?array $voteResults;
 
-
     /**
-     * @param UserUuid|null $userUuid
      * @param VotingUuid|null $votingUuid
+     * @param DateTime|null $startDate
+     * @param DateTime|null $finishDate
      * @param Subject|null $subject
      * @param array|null $voteResults
      */
     public function __construct(
-        private ?UserUuid   $userUuid = null,
         private ?VotingUuid $votingUuid = null,
+        private ?DateTime   $startDate = null,
+        private ?DateTime   $finishDate = null,
         private ?Subject    $subject = null,
         ?array              $voteResults = null
     )
     {
         $this->addListOfVoteResult($voteResults);
-    }
-
-    /**
-     * @return UserUuid|null
-     */
-    public function getUserUuid(): ?UserUuid
-    {
-        return $this->userUuid;
-    }
-
-    /**
-     * @param UserUuid|null $userUuid
-     */
-    public function setUserUuid(?UserUuid $userUuid): void
-    {
-        $this->userUuid = $userUuid;
     }
 
     /**
@@ -62,6 +47,38 @@ class Vote
     public function setVotingUuid(?VotingUuid $votingUuid): void
     {
         $this->votingUuid = $votingUuid;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getStartDate(): ?DateTime
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param DateTime|null $startDate
+     */
+    public function setStartDate(?DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getFinishDate(): ?DateTime
+    {
+        return $this->finishDate;
+    }
+
+    /**
+     * @param DateTime|null $finishDate
+     */
+    public function setFinishDate(?DateTime $finishDate): void
+    {
+        $this->finishDate = $finishDate;
     }
 
     /**
